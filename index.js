d40d3fac41ce75a7520986853dcc216b70a859d3
patrice mocombe <patricemocombe@gmail.com>
2022-04-05 11:33:58 -0400

    commit tree 11d3962924889d82c1b3b49d9b69ff9cda61d2b8

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *10);
+
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max= Math.max(5, 10, 15, 20);
+
+
